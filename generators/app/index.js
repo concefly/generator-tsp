5a4143311126d2c272b1db09b358a8ccbb8884c6
concefly <concefly@foxmail.com>
2019-08-04 23:43:26 +0800

    first commit

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,63 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const yosay = require('yosay');
+const path = require('path');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the fantabulous ${chalk.red('generator-tsp')} generator!`)
-    );
+  constructor(...args) {
+    super(...args);
+
+    this.answers = {
+      pkg: '',
+      gitName: '',
+      gitEmail: '',
+    };
+  }
+
+  async prompting() {
+    this.log(chalk.green('typescript npm package 生成器'));
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+        type: 'input',
+        name: 'pkg',
+        message: 'Your package name (no word-breaks)',
+        default: path.basename(this.determineAppname().replace(/ /g, '-')),
+      },
+      {
+        type: 'input',
+        name: 'gitName',
+        message: 'git name',
+        default: this.git && this.git.name(),
+      },
+      {
+        type: 'input',
+        name: 'gitEmail',
+        message: 'git email',
+        default: this.git && this.git.email(),
+      },
     ];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.answers = {
+      ...this.answers,
+      ...(await this.prompt(prompts)),
+    };
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
+    [
+      ['__tests__', '__tests__'],
+      ['src', 'src'],
+      ['_gitignore', '.gitignore'],
+      ['_jest.config.js', '.jest.config.js'],
+      ['_package.json', 'package.json'],
+      ['_travis.yml', '.travis.yml'],
+      ['_tsconfig.build.es.json', 'tsconfig.build.es.json'],
+      ['_tsconfig.build.json', 'tsconfig.build.json'],
+      ['_tsconfig.json', 'tsconfig.json'],
+      ['LICENSE', 'LICENSE'],
+    ].forEach(([a, b]) => {
+      this.fs.copyTpl(this.templatePath(a), this.destinationPath(b), this.answers);
+    });
   }
 };
